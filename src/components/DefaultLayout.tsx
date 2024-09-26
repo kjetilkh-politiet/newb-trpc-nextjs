10f38153453c947a5c902702f271278e27626bd4
Kjetil Kyrkjerud Hårtveit <kjetil.kyrkjerud.hartveit@politiet.no>
2024-09-26 13:04:56 +0200

    fix: title in <head>

--- src/components/DefaultLayout.tsx
+++ src/components/DefaultLayout.tsx
@@ -3,16 +3,18 @@ import type { ReactNode } from 'react';
 
 type DefaultLayoutProps = { children: ReactNode };
 
+const title = 'tRPC | Next.js';
+
 export const DefaultLayout = ({ children }: DefaultLayoutProps) => {
   return (
     <>
       <Head>
-        <title>Prisma Starter</title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="h-screen">
-        <h1 className="text-4xl mb-6">tRPC | Next.js</h1>
+        <h1 className="text-4xl mb-6">{title}</h1>
         {children}
       </main>
     </>
